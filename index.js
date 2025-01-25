3998739889d96bc93f5734122f086e000b42212b
va-yesa <164873037+va-yesa@users.noreply.github.com>
2025-01-25 16:56:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchform.classList.remove('active');
+    cartitem.classList.remove('active');
+}
+let searchform = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchform.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartitem.classList.remove('active');
+}
+
+let cartitem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartitem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchform.classList.remove('active');
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchform.classList.remove('active');
+    cartitem.classList.remove('active');
+}
